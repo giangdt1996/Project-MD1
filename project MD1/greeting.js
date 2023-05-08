ccdd2a6614cec7d66b9698b52b24e1d172729605
giangdt1996 <127973290+giangdt1996@users.noreply.github.com>
2023-05-08 08:09:31 +0700

    Delete project MD1 directory

--- project MD1/greeting.js
+++
@@ -1,9 +0,0 @@
-const userLogin = JSON.parse(localStorage.getItem("user_login"));
-let username = userLogin.name;
-if (username) {
-  var greeting = document.querySelector(".greeting");
-  greeting.innerHTML = `<a href="/history/history.html" style="text-decoration:none;color:black">XIN CHÀO ${username}</a>`;
-} else {
-  var greeting = document.querySelector(".greeting");
-  greeting.innerHTML = `<a id="btn-login-sigin" href="../login/login.html" class="loginout" target="_blank"><i class="fa-solid fa-circle-user"></i>ĐĂNG NHẬP/ĐĂNG KÝ</a>`;
-}
